f9575afa6f607bf0f9afe48bbf3b282a8ab2b961
DALTON LEYIAN <daltonleyian@gmail.com>
2024-04-20 11:24:20 +0300

    Initial Commit

---
+++ src/main/java/com/example/QhalaHackathon/QhalaHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.example.QhalaHackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QhalaHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QhalaHackathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/QhalaHackathon/QhalaHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QhalaHackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QhalaHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
